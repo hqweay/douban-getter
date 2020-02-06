689dceb3f6482b4da01e73af701b1c0b2cbd7fd1
hqweay <hqweay@gmail.com>
2020-02-06 19:56:17 +0800

    update: 备份豆瓣标记为看过的电影

--- example.js
+++ example.js
@@ -1,4 +1,4 @@
-let getWatchedMoviesBySpider = require('./markall-douban-watched-movie');
+let getWatchedMoviesBySpider = require('./markall-douban-watched-movies');
 // 存储路径
 const STORE_PATH = 'douban-data-backup/';
 
