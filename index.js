574b2ee43e14941e41f8a173ab42b8b5b2143b59
hqweay <hqweay@gmail.com>
2022-03-27 01:05:45 +0800

    update: getDoubanItemInfo

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-let createDoubanDataGetter = require('./src/douban-data-getter-func-creater');
+let { createDoubanDataGetter, getDoubanItemInfo } = require('./src/douban-data-getter-func-creater');
 let saveDoubanData = require('./src/save-douban-data-to-local');
 let DoubanTypeEnum = require('./src/config/doubanEnum');
 
 module.exports = {
   createDoubanDataGetter,
+  getDoubanItemInfo,
   saveDoubanData,
   DoubanTypeEnum,
 }
